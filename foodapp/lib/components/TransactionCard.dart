268689e4b26a54e1e7cdb189a6a9ae13ee40bfe7
ganeshgovala <sriramganesh28@gmail.com>
2024-09-02 21:16:11 +0530

    TransactionCard

---
+++ foodapp/lib/components/TransactionCard.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class TransactionCard extends StatelessWidget {
+  const TransactionCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: 50,
+        width: MediaQuery.of(context).size.width / 1.1,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                CircleAvatar(
+                  backgroundColor: Colors.yellow.shade700,
+                  child: Icon(Icons.person, color: const Color.fromARGB(255, 0, 0, 0))
+                ),
+                SizedBox(width: 10),
+                Text("Transaction Name", style : TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w800
+                )),
+              ],
+            ),
+            Text("\$12", style : TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold
+            ))
+          ],
+        )
+      ),
+    );
+  }
+}
--- foodapp/lib/pages/Wallet.dart
+++ foodapp/lib/pages/Wallet.dart
@@ -340,35 +340,6 @@ class _WalletPageState extends State<WalletPage> {
                   )
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: 50,
-                  width: MediaQuery.of(context).size.width / 1.1,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          CircleAvatar(
-                            backgroundColor: Colors.yellow.shade700,
-                            child: Icon(Icons.person, color: const Color.fromARGB(255, 0, 0, 0))
-                          ),
-                          SizedBox(width: 10),
-                          Text("Transaction Name", style : TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w800
-                          )),
-                        ],
-                      ),
-                      Text("\$12", style : TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold
-                      ))
-                    ],
-                  )
-                ),
-              ),
             ],
           ),
         ),
