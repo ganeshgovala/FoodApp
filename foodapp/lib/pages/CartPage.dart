5185eedff5cbcf7530d2cf7740effa527342f3b7
ganeshgovala <sriramganesh28@gmail.com>
2024-09-11 21:16:04 +0530

    ..

--- foodapp/lib/pages/CartPage.dart
+++ foodapp/lib/pages/CartPage.dart
@@ -29,7 +29,7 @@ class _CartPageState extends State<CartPage> {
                 itemCount: widget._cartModel.orderItems.length,
                 itemBuilder: (context, index) {
                   return Material(
-                    elevation: 5.0,
+                    elevation: 2.0,
                     borderRadius: BorderRadius.circular(10),
                     child: Container(
                       child: Row(
